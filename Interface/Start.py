bc5ab791e105160fa40cbb3005e39abbaf257078
Gautam Mundewadi <gautammundewadi@ucsb.edu>
2022-11-09 12:00:46 -0800

    gm - Interface outline working

--- Interface/Start.py
+++ Interface/Start.py
@@ -7,16 +7,30 @@ import cvlib as cv
 import cv2
 import torch
 
+
+# Check that MPS is available
+if not torch.backends.mps.is_available():
+    if not torch.backends.mps.is_built():
+        print("MPS not available because the current PyTorch install was not "
+              "built with MPS enabled.")
+    else:
+        print("MPS not available because the current MacOS version is not 12.3+ "
+              "and/or you do not have an MPS-enabled device on this machine.")
+            
+
 # open webcam
 webcam = cv2.VideoCapture(0)
-model = torch.load('../models/AlexNet_Scratch_dataAugment')
-model.eval()
+
 emotions = {0: 'angry', 1: 'happy', 2: 'neutral', 3: 'sad'}
 
 if not webcam.isOpened():
     print("Could not open webcam")
     exit()
-    
+
+mps_device = torch.device('mps')
+model = torch.load('../models/AlexNet_Scratch_dataAugment')
+model.eval()
+model = model.to(mps_device)
 
 # loop through frames
 while webcam.isOpened():
@@ -31,29 +45,35 @@ while webcam.isOpened():
     # apply face detection
     face, confidence = cv.detect_face(frame)
 
-    # crop frame using face and convert to tensor
-    outputs = model(face)
-    _, preds = torch.max(outputs, 1)
-
     # loop through detected faces
     for idx, f in enumerate(face):
         
         (startX, startY) = f[0], f[1]
         (endX, endY) = f[2], f[3]
 
+        # crop and resize image of face
+        img = cv2.resize(frame[startY:endY,startX:endX], (224, 224), interpolation = cv2.INTER_AREA)
+
+        # make prediction
+        input = torch.tensor(img).view(-1, 3, 224, 224).float()
+        input = input.to(mps_device)
+
+        output = model(input)
+        _, preds = torch.max(output, 1)
+        print(preds)
+
         # draw rectangle over face
         cv2.rectangle(frame, (startX,startY), (endX,endY), (0,255,0), 2)
-
-        text = "{:.2f}%".format(confidence[idx] * 100)
+        text = f'{emotions[preds.item()]}'
 
         Y = startY - 10 if startY - 10 > 10 else startY + 10
 
         # write confidence percentage on top of face rectangle
         cv2.putText(frame, text, (startX,Y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
-                    (0,255,0), 2)
+                    (0,255,0), 3)
 
     # display output
-    cv2.imshow("Real-time face detection", frame)
+    cv2.imshow("Real-time emotions detection", frame)
 
     # press "Q" to stop
     if cv2.waitKey(1) & 0xFF == ord('q'):
