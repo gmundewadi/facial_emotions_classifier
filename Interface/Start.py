163c34ca6d6787a9f6db45a2fdc07ce6959684dc
Gautam Mundewadi <gautammundewadi@ucsb.edu>
2022-11-18 15:37:35 -0800

    gm - mobilenetv2 (overfit) and live bar chart interface

--- Interface/Start.py
+++ Interface/Start.py
@@ -8,6 +8,9 @@ import cvlib as cv
 import cv2
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
 
 # Check that MPS is available
 if not torch.backends.mps.is_available():
@@ -21,20 +24,25 @@ if not torch.backends.mps.is_available():
 
 # open webcam
 webcam = cv2.VideoCapture(0)
+fig = plt.figure()
 
 emotions = {0: 'angry', 1: 'happy', 2: 'neutral', 3: 'sad'}
+emotion_values = ['angry', 'happy', 'neutral', 'sad']
 
 if not webcam.isOpened():
     print("Could not open webcam")
     exit()
 
 mps_device = torch.device('mps')
-model = torch.load('../models/AlexNet_TL_dataAugment')
+model = torch.load('../models/AlexNet_Scratch_50epochs')
 model.eval()
 model = model.to(mps_device)
 
 # loop through frames
 while webcam.isOpened():
+
+    width  = webcam.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
+    height = webcam.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
 
     # read frame from webcam 
     status, frame = webcam.read()
@@ -53,27 +61,39 @@ while webcam.isOpened():
         (endX, endY) = f[2], f[3]
 
         l = max(endX - startX, endY - startY)
-
         leftAdjust = 40
-
         cropped_face = frame[startY:startY + l,startX - leftAdjust : startX + l-leftAdjust]
         
         # crop and resize image of face
         img = cv2.resize(cropped_face, (224, 224), interpolation=cv2.INTER_AREA)
-
-        cv2.imwrite('./live_img.png', img)
-
-        #TODO: figure out cropping (maybe get a larger image and crop that down)
 
         # make prediction
         input = torch.tensor(img).view(-1, 3, 224, 224).float()
         input = input.to(mps_device)
 
         output = model(input)
-        print(output)
-        _, preds = torch.max(output, 1)
+        softmax = torch.nn.Softmax(dim = 1)
+        probabilites = softmax(output)
 
-        # draw rectangle over face
+        # draw rectangle and prediction over face
+        _, preds = torch.max(probabilites, 1)
+
+        # generate bar chart of probabilities. Set the color of the prediction as blue
+        plt.clf()
+        colors = ['grey'] * 4
+        colors[preds.item()] = 'blue'
+        barChartFig = plt.bar(emotion_values,probabilites.cpu().detach().numpy()[0], color = colors, width=.4)
+        plt.xlabel('Emotion')
+        plt.ylabel('Probability')
+        
+        # display matplotlib fig using opencv
+        fig.canvas.draw()
+
+        # img is rgb, convert to opencv's default bgr
+        barChartImg = cv2.cvtColor(np.asarray(fig.canvas.buffer_rgba()), cv2.COLOR_RGBA2BGR)
+        # cv2.resizeWindow(barChartImg, (int(width*.2), int(height*.2)))
+        
+    
         cv2.rectangle(frame, (startX,startY), (endX,endY), (0,255,0), 2)
         text = f'{emotions[preds.item()]}'
 
@@ -83,8 +103,10 @@ while webcam.isOpened():
         cv2.putText(frame, text, (startX,Y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
                     (0,255,0), 3)
 
-    # display output
+    # display frames and bar chart
     cv2.imshow("Real-time emotions detection", frame)
+    cv2.imshow("Probabilites", barChartImg)
+
 
     # press "Q" to stop
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- MobileNet/MobileNetV2TL.py
+++ MobileNet/MobileNetV2TL.py
@@ -1,11 +1,9 @@
 """
-VGG16TL.py
-Transfer learning using VGG16 to predict facial emotions. 
+MobileNetV2TL.py
+Transfer learning using MobileNetV2 to predict facial emotions. 
 Utilizes Metal Performance Shaders for quick training/validation on Apple Silicon
 Author: Gautam Mundewadi
 """
-
-import random
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,8 +11,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
-from torchvision import datasets, models, transforms
-from torchvision.models import VGG16_Weights
+from torchvision import datasets, transforms, models
+from torchvision.models import MobileNet_V2_Weights
 
 # Check that MPS is available
 if not torch.backends.mps.is_available():
@@ -25,68 +23,57 @@ if not torch.backends.mps.is_available():
         print("MPS not available because the current MacOS version is not 12.3+ "
               "and/or you do not have an MPS-enabled device on this machine.")
             
-train_transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1, hue=0.1),
-        transforms.RandomAffine(degrees=40, translate=None, scale=(1, 2), shear=15),
-        transforms.ToTensor(),
-        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-])
-
-test_transform = transforms.Compose([
+transform = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 ])
 
-# Load dataset and perform transformations
-train_dataset = datasets.ImageFolder('../dataset/train', transform = train_transform)
-test_dataset = datasets.ImageFolder('../dataset/test', transform = test_transform)
+# Load dataset and perform transformations (same transforms for train & test)
+train_dataset = datasets.ImageFolder('../dataset/train', transform = transform)
+test_dataset = datasets.ImageFolder('../dataset/test', transform = transform)
 
 # Dataloader iteratble returns batches of images and corresponding labels
-batchSize = 256 #TODO: consider other batch sizes
+batchSize = 32
+trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=batchSize, shuffle=True)
 testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batchSize, shuffle=True)
 
-num_training_points = len(train_dataset) 
-test_dataset_size = len(testloader) # number of test datapoints per batch
-
+train_dataset_size = len(trainloader)
+test_dataset_size = len(testloader)
 
 mps_device = torch.device('mps')
 
-VGG16_model = models.vgg16(weights = VGG16_Weights.DEFAULT)
-VGG16_model.eval()
+mobileNetV2_model = models.mobilenet_v2(weights = MobileNet_V2_Weights.DEFAULT)
+mobileNetV2_model.eval()
 
 #Updating the third and the last classifier that is the output layer of the network. Make sure to have 4 output nodes if we are going to get 4 class labels through our model.
-VGG16_model.classifier[6] = nn.Linear(4096,4)
-VGG16_model.to(mps_device)
+mobileNetV2_model.classifier[1] = nn.Linear(mobileNetV2_model.last_channel, 4)
+
+mobileNetV2_model.to(mps_device)
 
 # Loss
 criterion = nn.CrossEntropyLoss()
 
 # Optimizer(SGD)
-optimizer = optim.SGD(VGG16_model.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(mobileNetV2_model.parameters(), lr=0.001, momentum=0.9)
 
 # learning rate decreases step-wise by a factor of .1 ~every 10K iterations
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
 
-niters = 200 # niters 
+nepochs = 20
 
-iters = list(range(1, niters+1))
+epochs = list(range(1, nepochs+1))
 train_loss_trend, train_acc_trend, test_loss_trend, test_acc_trend = [], [], [], []
 
-for iter in range(niters): 
+for epoch in range(nepochs): 
     
     running_loss = 0.0
     running_accuracy = 0.0
 
-    subset = torch.utils.data.Subset(train_dataset, [random.randint(0, num_training_points-1)]) # select 1 datapoint for SGD
-    trainloader_stochastic = torch.utils.data.DataLoader(subset, batch_size=1, num_workers=0, shuffle=False)
-    
-    for i, data in enumerate(trainloader_stochastic, 0):
+    for i, data in enumerate(trainloader, 0):
         # load input and labels into gpu
         inputs, labels = data[0], data[1]
-
         inputs = inputs.to(mps_device)
         labels = labels.to(mps_device)
 
@@ -94,7 +81,7 @@ for iter in range(niters):
         optimizer.zero_grad()
 
         # make predictions & calc accuracy
-        outputs = VGG16_model(inputs)
+        outputs = mobileNetV2_model(inputs)
         _, preds = torch.max(outputs, 1)
         running_accuracy += float(torch.sum(preds == labels)) / batchSize 
 
@@ -105,11 +92,11 @@ for iter in range(niters):
 
         running_loss += loss.item()
 
-    train_loss_this_iter = running_loss
-    train_acc_this_iter = running_accuracy 
+    train_loss_this_epoch = running_loss / train_dataset_size
+    train_acc_this_epoch = running_accuracy / train_dataset_size
 
-    train_loss_trend.append(train_loss_this_iter)
-    train_acc_trend.append(train_acc_this_iter)
+    train_loss_trend.append(train_loss_this_epoch)
+    train_acc_trend.append(train_acc_this_epoch)
 
     running_loss = 0.0
     running_accuracy = 0.0
@@ -121,40 +108,38 @@ for iter in range(niters):
         labels = labels.to(mps_device)
 
         # make predictions & calc accuracy 
-        outputs = VGG16_model(inputs)
+        outputs = mobileNetV2_model(inputs)
         _, preds = torch.max(outputs, 1)
         running_accuracy += float(torch.sum(preds == labels)) / batchSize
 
         # compute running loss
         loss = criterion(outputs, labels)
         running_loss += loss.item()
-        print(f'Evaluation step: {i} / {test_dataset_size}')
 
-    test_loss_this_iter = running_loss / test_dataset_size
-    test_acc_this_iter = running_accuracy / test_dataset_size
+    test_loss_this_epoch = running_loss / test_dataset_size
+    test_acc_this_epoch = running_accuracy / test_dataset_size
     
-    test_loss_trend.append(test_loss_this_iter)
-    test_acc_trend.append(test_acc_this_iter)
+    test_loss_trend.append(test_loss_this_epoch)
+    test_acc_trend.append(test_acc_this_epoch)
 
-    print(f'Iteration {iter + 1} Training Loss: {train_loss_this_iter:.4f} Training Acc: {train_acc_this_iter:.4f} Test Loss: {test_loss_this_iter:.4f} Test Acc: {test_acc_this_iter:.4f}')\
+    print(f'Epoch {epoch + 1} Training Loss: {train_loss_this_epoch:.4f} Training Acc: {train_acc_this_epoch:.4f} Test Loss: {test_loss_this_epoch:.4f} Test Acc: {test_acc_this_epoch:.4f}')\
     
-
 
 f,ax=plt.subplots(2,1,figsize=(10,10)) 
 
 #Assigning the first subplot to graph training loss and test loss
-ax[0].plot(iters, train_loss_trend,color='b',label='Training Loss')
-ax[0].plot(iters, test_loss_trend,color='r',label='Test Loss')
-ax[0].set(xlabel='Iteration', ylabel = 'Loss')
+ax[0].plot(epochs, train_loss_trend,color='b',label='Training Loss')
+ax[0].plot(epochs, test_loss_trend,color='r',label='Test Loss')
+ax[0].set(xlabel='Epoch', ylabel = 'Loss')
 ax[0].legend()
 
 #Plotting the training accuracy and test accuracy
-ax[1].plot(iters,train_acc_trend,color='b',label='Training Accuracy')
-ax[1].plot(iters,test_acc_trend,color='r',label='Test Accuracy')
-ax[1].set(xlabel='Iteration', ylabel = 'Accuracy')
+ax[1].plot(epochs,train_acc_trend,color='b',label='Training Accuracy')
+ax[1].plot(epochs,test_acc_trend,color='r',label='Test Accuracy')
+ax[1].set(xlabel='Epoch', ylabel = 'Accuracy')
 ax[1].legend()
 
-plt.savefig('../graphs/VGG16_TL.png')
-torch.save(VGG16_model, '../models/VGG16_TL')
+plt.savefig('../graphs/MobileNetV2_TL.png')
+torch.save(mobileNetV2_model, '../models/MobileNetV2')
 
 print('Accuracy Score = ', np.max(test_acc_trend))
